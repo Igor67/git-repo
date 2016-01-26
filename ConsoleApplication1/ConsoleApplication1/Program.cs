e0c1529098d82da8affc01219a3ffe4a6cdeb6a4
igorzab <igorzab22@gmail.com>
2016-01-26 10:19:00 +0200

    h

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world;55");
+            Console.WriteLine("Hello world;55gjkhl");
         }
     }
 }
